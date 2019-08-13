11b8446c9dace9dc7089c604fff55fd5103b4461
Michael Claborn <mclaborn2010@gmail.com>
2019-08-13 15:19:34 -0500

    some changes

--- app2.js
+++ app2.js
@@ -1,19 +1,43 @@
 let button = document.getElementById("next");
-let profileDisplay = document.getElementById("profileDisplay");
-let random = 1;
+let name = document.getElementById("name");
+let hairColor = document.getElementById("hair-color");
+let birthYear = document.getElementById("birth-year");
+let homeworld = document.getElementById("homeworld");
+let increment = 1;
 
 function generateName() {
-  fetch(`https://swapi.co/api/people/${random}`)
+  fetch(`https://swapi.co/api/people/${increment}`)
     .then(res => res.json())
-    .then(jsonRes => (profileDisplay.innerText = jsonRes.name))
+    .then(jsonRes => (name.innerText = jsonRes.name))
+    .catch(error => console.log("some error happen", error));
+}
+function generateHairColor() {
+  fetch(`https://swapi.co/api/people/${increment}`)
+    .then(res => res.json())
+    .then(jsonRes => (hairColor.innerText = jsonRes.hair_color))
+    .catch(error => console.log("some error happen", error));
+}
+function generateBirthYear() {
+  fetch(`https://swapi.co/api/people/${increment}`)
+    .then(res => res.json())
+    .then(jsonRes => (birthYear.innerText = jsonRes.birth_year))
+    .catch(error => console.log("some error happen", error));
+}
+function generateHomeWorld() {
+  fetch(`https://swapi.co/api/planets/${increment}`)
+    .then(res => res.json())
+    .then(jsonRes => (homeworld.innerText = jsonRes.homeworld))
     .catch(error => console.log("some error happen", error));
 }
 function moveForward() {
-  random++;
-  console.log(random);
+  increment++;
+  console.log(increment);
 }
 
 button.addEventListener("click", () => {
   generateName();
+  generateBirthYear();
+  generateHairColor();
+  generateHomeWorld();
   moveForward();
 });
